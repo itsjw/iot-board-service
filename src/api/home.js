d367b5babb94664fcbdc4b8b9d715dac37244a47
Jei <jeiyang108@gmail.com>
2017-11-08 19:41:31 -0800

    Added endpoint to access details for user.

--- src/api/home.js
+++ src/api/home.js
@@ -1,15 +1,29 @@
 import resource from 'resource-router-middleware';
 
-import nestModel from './../models/nest';
+import HomeModel from './../models/home';
+import UserModel from './../models/user';
 
 export default ({ config, db }) => resource({
-	id : 'device',
-        
-	load(req, id, callback) {
-		callback(null, id);
-	},
-	/** GET /:id - Return a given entity */
-	read({ device }, res) {
-		res.json(nestModel.queryLatest(device));
-	}
+        id: 'user',
+
+        load(req, id, callback) {
+            console.log(id);
+            callback(null, id);
+        },
+
+        read( { user }, res) {
+            new HomeModel(config).queuryRooms(user).then((response) => {
+                res.json(response);
+            }).catch((err) => {
+                res.json({isError: true, errorDescription: err.toString()});
+            });
+        },
+
+        index( { params }, res) {
+            new UserModel(config).getUsers().then((result) => {
+                res.json(result);
+            }).catch((err) => {
+                throw err;
+            });
+        }
     });
